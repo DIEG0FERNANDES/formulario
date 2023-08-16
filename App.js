d38ad39e3d5b588f55a495be809de44fa1a02290
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2023-08-15 23:55:43 -0400

    commit inicial

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, TextInput, View, Button, Text } from 'react-native';
+import { useState } from 'react-native';
 
 export default function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const calcIMC = () => {
+    const IMC = weight / (height * height);
+    const IMCFormulado = IMC.toFixed(2);
+    setResult(+IMCFormulado);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View >
+        <TextInput
+          placeholder="Peso (kilos)"
+          value={weight}
+          style={styles.input}
+          onChange={(e) => setWeight(+e.target.value)}
+        />
+        <TextInput
+          placeholder="Altura (Centimetros)"
+          value={height}
+          onChange={(e) => setHeight(+e.target.value)}
+          style={styles.input}
+        />
+      </View>
+      <Button
+        onPress={calcIMC}
+        title="Calcular"
+        color="#841584"
+      />
+      <Text>{result}</Text>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +44,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 10,
   },
 });
